19d71f2667b2333b6dafbb30e50a88028356b6b0
M Noman Rauf <noamn.rauf90@gmail.com>
2023-07-24 11:54:23 +0500

    firebase installation and github actions setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBe-lkiDcRv9YZ4-SMHcLmaPCbx-4Xm22g",
+    authDomain: "work-journal-app.firebaseapp.com",
+    projectId: "work-journal-app",
+    storageBucket: "work-journal-app.appspot.com",
+    messagingSenderId: "356080434312",
+    appId: "1:356080434312:web:9e1d1eed3fa78d554a733d",
+    measurementId: "G-7FEWL985MX"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBe-lkiDcRv9YZ4-SMHcLmaPCbx-4Xm22g",
+    authDomain: "work-journal-app.firebaseapp.com",
+    projectId: "work-journal-app",
+    storageBucket: "work-journal-app.appspot.com",
+    messagingSenderId: "356080434312",
+    appId: "1:356080434312:web:9e1d1eed3fa78d554a733d",
+    measurementId: "G-7FEWL985MX"
+  }
 };
 
 /*
